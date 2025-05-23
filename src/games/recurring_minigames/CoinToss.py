295861fde0aeb884251163c612c2d78e2fd741a8
msxgit <mikrosx2000@gmail.com>
2025-05-23 12:21:30 +0100

    Added Recurring Games

---
+++ src/games/recurring_minigames/CoinToss.py
@@ -0,0 +1,18 @@
+import random
+from games.recurring_minigames import RecurringMinigame
+class CoinTossDaily(RecurringMinigame):
+
+    _options: list[str] 
+    def __init__(self) -> None:
+        self._options = ["Tails","Heads"]
+        self._win_amount = 50
+        self._refresh_period_seconds = 60 * 60 * 24
+
+    def _determine_win(self, player_pick: str) -> str:
+        if player_pick not in self._options:
+            raise ValueError(f"Player pick is not one of the options, Player picked: {player_pick}, expected {self._options}")
+        
+        result = random.choice(self._options)
+        if result == player_pick:
+            return "Win"
+        return "Lose"
---
+++ src/games/recurring_minigames/RecurringMinigame.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+
+class RecurringMinigame(ABC):
+    _win_amount: int
+    _refresh_period_seconds: int
+    
+    def __init__(self) -> None:
+        pass
+
+    @abstractmethod
+    def _determine_win(self, player_pick: str) -> str:
+        '''
+            Determine whever the loses wins or draws
+        '''
+        pass
