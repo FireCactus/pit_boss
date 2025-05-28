f5337829defba55641738406044e22001fe9e520
FireCactus <ipadowski123@gmail.com>
2025-05-28 19:50:39 +0200

    added utilities for interacting with chat

--- src/games/Game.py
+++ src/games/Game.py
@@ -12,54 +12,6 @@ class Game(ABC):
 
     def __init__(self) -> None:
         pass
-
-    def init_game_statistics(self) -> None:
-        ''' initializes all tracked statistics at 0'''
-        for player_name in self.players:
-            for statistic_name in self._tracked_statistics:
-                self._player_statistics[player_name][statistic_name] = 0
-
-    
-    def modify_player_statistic(self, player_name: str, statiscic_name: str, operation: StatisticOperations, amount:int) -> None:
-        if operation == StatisticOperations.ADD:
-            self._player_statistics[player_name][statiscic_name] += amount
-        
-        elif operation == StatisticOperations.SUBTRACT:
-            self._player_statistics[player_name][statiscic_name] -= amount
-        
-        elif operation == StatisticOperations.SET_TO:
-            self._player_statistics[player_name][statiscic_name] = amount
-    
-
-    async def send_vanishing_message(self, string: str) -> Message:
-        ''' 
-        Sends a text message through discord that will delete after _delete_message_after and returns the message object
-        '''
-        return await ctx.send(string, delete_after=self._delete_message_after)
-
-
-    async def send_persistant_message(self, string: str) -> Message:
-        ''' 
-        Sends a text persistant message through discord nad returns the message object
-        '''
-        return await ctx.send(string)
-    
-
-    async def edit_message(self, message: Message, string: str) -> Message:
-        ''' 
-        Sends a text persistant message through discord nad returns the message object
-        '''
-        return await message.edit(content=string)
-    
-
-    async def delete_message(self, message: Message) -> None:
-        ''' 
-        Tries to delete the provided message
-        '''
-        try:
-            await message.delete()
-        except discord.NotFound:
-            pass #if failed to delete a message because it doesnt exist then dont throw an error
     
     @abstractmethod
     async def play(self) -> None:
