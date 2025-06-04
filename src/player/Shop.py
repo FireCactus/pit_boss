c09c943c2d1b0a6a5bd8ca3d0c6dd81f53f8372a
FireCactus <ipadowski123@gmail.com>
2025-06-04 15:27:32 +0200

    temp fix for shop to have unlimited items

--- src/player/Shop.py
+++ src/player/Shop.py
@@ -15,22 +15,26 @@ from games.scratch_off.XMarksTheSpot import XMarksTheSpot
 
 db = PlayersDatabase()
 
-class ShopItem(NamedTuple):
+class UnlimitedShopItem(NamedTuple):
+    item_type: Type[Item]
+
+class LimitedShopItem(NamedTuple):
     item_type: Type[Item]
     restock_probability: float # from 0 to 1, the probability of the item appearing in stock 
     restock_min: int # minumu amount of this item in the restock
     restock_max: int # maximum amount of this item in the restock
+    
     
 
 class Shop:
 
     def __init__(self) -> None:
         self.possible_shop_items: list[ShopItem] = [
-            ShopItem(SuperPayout,     0.8, 2, 6),
-            ShopItem(EmojiLines,      0.6, 1, 5),
-            ShopItem(XMarksTheSpot,   0.9, 3, 8),
-            ShopItem(TransportSearch, 0.5, 1, 4),
-            ShopItem(DiamondRush,     0.4, 2, 4),
+            UnlimitedShopItem(SuperPayout),
+            UnlimitedShopItem(EmojiLines),
+            UnlimitedShopItem(XMarksTheSpot),
+            UnlimitedShopItem(TransportSearch),
+            UnlimitedShopItem(DiamondRush)
         ]
 
     def get_items_in_stock(self) -> list[Item]:
@@ -44,26 +48,51 @@ class Shop:
                     items.append(item)
             except (FileNotFoundError, pickle.UnpicklingError) as e:
                 print(f"Error while unpickling object: {pickle_path}:\n{e}")
-
+        
         return items
     
     def delete_item_from_stock(self, item: Item) -> None:
         db.delete_item_from_player_inventory(item.get_filepath())
         item.delete_from_disk()
-    
-    def restock(self) -> None:
 
-        for item in self.get_items_in_stock():
-            self.delete_item_from_stock(item)
+    def restock_unlimited_shop_items(self, delete_old_items: bool = True) -> None:
+        
+        if delete_old_items:
+            for item in self.get_items_in_stock():
+                for shop_item in self.possible_shop_items:
+                    if isinstance(item, shop_item.item_type) and isinstance(shop_item, UnlimitedShopItem):
+                        self.delete_item_from_stock(item)
+                        break
 
         for possible_item in self.possible_shop_items:
-            rand: float = random.random()
-            if possible_item.restock_probability < rand:
+            if not isinstance(possible_item, UnlimitedShopItem):
+                continue
+
+            item = possible_item.item_type()
+            item.save_to_disk()
+            db.save_item_to_player_inventory(db._shop_discord_id, item.get_filepath())
+    
+    def restock_limited_shop_items(self) -> None:
+        
+
+        for item in self.get_items_in_stock():
+            for item in self.get_items_in_stock():
+                for shop_item in self.possible_shop_items:
+                    if isinstance(item, shop_item.item_type) and isinstance(shop_item, LimitedShopItem):
+                        self.delete_item_from_stock(item)
+
+        for possible_item in self.possible_shop_items:
+            
+            if not isinstance(possible_item, LimitedShopItem):
+                continue
+
+            if possible_item.restock_probability < random.random():
                 continue
 
             item_amount: int = random.randint(possible_item.restock_min, possible_item.restock_max)
 
             for _ in range(item_amount):
+                
                 item = possible_item.item_type()
                 item.save_to_disk()
                 db.save_item_to_player_inventory(db._shop_discord_id, item.get_filepath())
